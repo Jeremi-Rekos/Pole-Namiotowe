ca5220003c8a1a5857e28206507756b564539a49
Jeremi Rękoś <rekos.jeremi@gmail.com>
2023-11-02 20:46:04 +0100

    Galeria general change

--- Main/Galeria.js
+++ Main/Galeria.js
@@ -1,10 +1,23 @@
-var lightbox = GLightbox();
-lightbox.on("open", (target) => {
-  console.log("lightbox opened");
-});
-var lightboxDescription = GLightbox({
-  selector: ".glightbox2",
-});
-var lightboxVideo = GLightbox({
-  selector: ".glightbox3",
-});
+const test1 = document.getElementById("card1");
+var test2 = document.getElementById("details1");
+
+test1.addEventListener("mouseover", test3);
+test1.addEventListener("mouseout", test4);
+
+function test3() {
+  test2.style.bottom = "0";
+}
+function test4() {
+  test2.style.bottom = "-50px";
+}
+
+var item = document.querySelectorAll("card");
+var name = item + [i];
+for (var i = 0; i < item.length; i++) {
+  function test3() {
+    name.style.bottom = "0";
+  }
+  function test4() {
+    name.style.bottom = "-50px";
+  }
+}
