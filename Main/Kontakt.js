ff6862b15ebcdec9c4cae6ca252e2ee7b0c363f9
Jeremi Rękoś <rekos.jeremi@gmail.com>
2023-11-06 20:41:29 +0100

    RWD kontakt Side //End

---
+++ Main/Kontakt.js
@@ -0,0 +1,30 @@
+const numImages = 4;
+
+let currentImageIndex = 1;
+const imageElement = document.getElementById("image");
+
+function autoSlide() {
+  currentImageIndex = (currentImageIndex % numImages) + 1;
+  showImage(currentImageIndex);
+}
+
+const slideInterval = setInterval(autoSlide, 7000);
+
+function showImage(index) {
+  const newImage = new Image();
+  newImage.src = `zdj/img${index}.jpg`;
+
+  newImage.onload = () => {
+    imageElement.style.transition = "opacity 0.5s";
+    imageElement.style.opacity = 0;
+
+    setTimeout(() => {
+      imageElement.src = newImage.src;
+    }, 500);
+
+    setTimeout(() => {
+      imageElement.style.opacity = 1;
+    }, 500);
+  };
+}
+showImage(currentImageIndex);
