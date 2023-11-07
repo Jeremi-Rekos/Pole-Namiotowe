f7ce0b72ededef162670b3af94fc0b0b53c0a491
Jeremi Rękoś <rekos.jeremi@gmail.com>
2023-11-07 21:12:53 +0100

    Galeria js end / lightbox start

--- Main/Galeria.js
+++ Main/Galeria.js
@@ -1,23 +1,22 @@
-const test1 = document.getElementById("card1");
-var test2 = document.getElementById("details1");
+var cards = document.querySelectorAll(".card");
+var display = document.getElementById("lightBoxDisplay");
 
-test1.addEventListener("mouseover", test3);
-test1.addEventListener("mouseout", test4);
-
-function test3() {
-  test2.style.bottom = "0";
-}
-function test4() {
-  test2.style.bottom = "-50px";
+function showUp() {
+  var details = this.querySelector(".details");
+  details.style.bottom = "0";
 }
 
-var item = document.querySelectorAll("card");
-var name = item + [i];
-for (var i = 0; i < item.length; i++) {
-  function test3() {
-    name.style.bottom = "0";
-  }
-  function test4() {
-    name.style.bottom = "-50px";
-  }
+function showDown() {
+  var details = this.querySelector(".details");
+  details.style.bottom = "-50px";
 }
+
+function lightBox() {
+  display.style.display = "flex";
+}
+
+cards.forEach(function (card) {
+  card.addEventListener("mouseover", showUp);
+  card.addEventListener("mouseout", showDown);
+  card.addEventListener("click", lightBox);
+});
